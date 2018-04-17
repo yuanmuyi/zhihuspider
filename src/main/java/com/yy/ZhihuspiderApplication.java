158c15bc9a09e1baef4d84032aabb088f534fec3
yuanyang <yang.yuan@biz-cloud.com.cn>
2018-04-17 22:17:01 +0800

    init

---
+++ src/main/java/com/yy/ZhihuspiderApplication.java
@@ -0,0 +1,12 @@
+package com.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuspiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhihuspiderApplication.class, args);
+	}
+}
