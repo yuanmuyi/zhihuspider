4347b2bd67b223d2caf79786f623a6b8dcd119c2
jjwhnd <598335117@qq.com>
2018-05-31 17:40:39 +0800

    add spider package

---
+++ src/main/java/com/yy/spider/pipeline/EsPipeline.java
@@ -0,0 +1,19 @@
+package com.yy.spider.pipeline;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+/**
+ * @author yuanyang
+ * @Description:es管道
+ * @date 2018/5/31 17:28
+ */
+public class EsPipeline implements Pipeline {
+
+
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+
+    }
+}
---
+++ src/main/java/com/yy/spider/processor/ZhihuPageProcessor.java
@@ -0,0 +1,22 @@
+package com.yy.spider.processor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * @author yuanyang
+ * @Description:知乎页面处理
+ * @date 2018/5/31 17:37
+ */
+public class ZhihuPageProcessor implements PageProcessor {
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
